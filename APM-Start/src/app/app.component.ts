635d25b399e000d8b2cc45e9242d7bb56b1957d8
Pedro Miyake <33829925+pedromiyake@users.noreply.github.com>
2021-09-21 10:34:37 +0200

    Installed bootstrap and font-awesome

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>
+        Welcome to {{pageTitle}}!!
+      </h1>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
